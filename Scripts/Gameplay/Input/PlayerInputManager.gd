d56adf9a05232b55c2aaf9723b72988b35bfcbad
VerhelstJoris <megajorver@gmail.com>
2025-06-29 14:53:32 +0100

    clean up input manager a bit

--- Scripts/Gameplay/Input/PlayerInputManager.gd
+++ Scripts/Gameplay/Input/PlayerInputManager.gd
@@ -10,17 +10,17 @@ func _input(event: InputEvent) -> void:
 		_player._on_mouse_motion_input(event)
 		return
 		
-	if(_player.input_receiver != null):	
-		for key in _player.input_receiver.input_dictionary:
-			if(event.is_action(key.input_string)):
-				_try_send_signal(event,_player.input_receiver,_player.input_receiver.input_dictionary[key])
-				
-	if(_player._can_use_equipment()):
-		for equip_key in _player.current_equipment.input_receiver.input_dictionary:
-			if(event.is_action(equip_key.input_string)):
-				_try_send_signal(event, _player.current_equipment.input_receiver,_player.current_equipment.input_receiver.input_dictionary[equip_key])
-				
+	if(_player.input_receiver != null):
+		_process_input_receiver(_player.input_receiver, event)
 		
+	if(_player._can_use_equipment() && _player.current_equipment.input_receiver):
+		_process_input_receiver(_player.current_equipment.input_receiver, event)
+		
+func _process_input_receiver(receiver : InputReceiver, event : InputEvent) -> void:
+	for key in receiver.input_dictionary:
+			if(event.is_action(key.input_string)):
+				ExposedSignalConnector._try_send_signal(receiver,receiver.input_dictionary[key], event)
+			
 func _try_send_signal(event: InputEvent, starting_node : Node ,signal_to_emit : ExposedSignalConnector ):
 	var found_node : Node = starting_node.get_node(signal_to_emit.signal_node)
 	if found_node:
--- Scripts/Helpers/ExposedSignalConnector.gd
+++ Scripts/Helpers/ExposedSignalConnector.gd
@@ -2,3 +2,12 @@ class_name ExposedSignalConnector extends  Resource
 
 @export	var signal_node : NodePath
 @export var signal_name : String
+
+static func _try_send_signal(starting_node : Node ,signal_to_emit : ExposedSignalConnector, params = {}) -> bool:
+	var found_node : Node = starting_node.get_node(signal_to_emit.signal_node)
+	
+	if found_node:
+		return found_node.emit_signal(signal_to_emit.signal_name, params) != ERR_UNAVAILABLE
+	else:
+		printerr("Node to send signal to does not exist")
+		return false
