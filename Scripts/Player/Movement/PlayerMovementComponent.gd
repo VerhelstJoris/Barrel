b403c026bd86dbad722cd8b55aaa1c4916e0e4e0
VerhelstJoris <megajorver@gmail.com>
2025-10-04 23:41:17 +0100

    first version of stepping up stairs logic

--- Scripts/Player/Movement/PlayerMovementComponent.gd
+++ Scripts/Player/Movement/PlayerMovementComponent.gd
@@ -17,7 +17,12 @@ signal on_player_movement_state_leave(new_state : PlayerStateMachine.E_StateName
 var current_horizontal_velocity : Vector2 = Vector2.ZERO
 
 @export_group("Step Height Settings")
-var max_step_up_height : float = 0.5
+@export var max_step_up_height : float = 0.26
+@onready var step_ray_forward : RayCast3D = %StepForwardRay
+@onready var step_ray_down : RayCast3D = %StepDownRay
+var _stepped_last_frame : bool = false
+
+var _draw_step_debug : bool = true
 
 @export_group("Gravity Settings")
 @export var max_gravity_velocity : float = -18
@@ -26,7 +31,6 @@ var max_step_up_height : float = 0.5
 
 var current_gravity_velocity : float = 0
 var current_gravity_time : float = 0
-
 
 const MOVE_FORWARD: String = "move_forward"
 const MOVE_BACK: String = "move_back"
@@ -68,26 +72,71 @@ func _process(delta: float) -> void:
 	state_machine._update(delta)	
 
 func _physics_process(_delta: float) -> void:
+	current_queued_velocity = Vector3.ZERO
 	state_machine._physics_update(_delta)
 	var current_active_state : PlayerState = state_machine._get_current_active_state()
 	_handle_player_move(_delta,current_active_state)
 	_handle_gravity(_delta, current_active_state)
-	
-	if(input_direction != Vector2.ZERO):
-		_try_step_up()
-		
-	player.velocity = current_queued_velocity
-	current_queued_velocity = Vector3.ZERO
-	
-	player.move_and_slide()
-	
-func _try_step_up() -> bool:
-	var step : Dictionary = PlayerCharacterStep.step_up(player.get_rid(), player.global_transform, max_step_up_height, Vector3(player.velocity.x, 0.0, player.velocity.z).normalized(), collision_shape.shape.radius * 0.5, 0.25)
-	if !step.is_empty() and step["normal"].angle_to(Vector3.UP) <= player.floor_max_angle:
-		player.global_position.y = step["point"].y
-		return true
-	return false	
 
+	var stepped_up : bool = _try_stair_step_up(_delta)
+	if(!stepped_up):
+		player.velocity = current_queued_velocity
+		player.move_and_slide()
+	
+	_stepped_last_frame = stepped_up
+
+func _is_surface_too_steep(normal : Vector3) -> bool:
+	return normal.angle_to(Vector3.UP) > player.floor_max_angle
+
+func _try_stair_step_up(_delta : float) -> bool:
+	if (!player.is_on_floor() && !_stepped_last_frame):
+		return false
+
+	if(player.velocity.y > 0):
+		return false
+		
+	# Not moving or attempting to move, skip stair check
+	if (current_queued_velocity.is_zero_approx() && input_direction.is_zero_approx()):
+		return false
+		
+	var expected_move_motion : Vector3 = Vector3(current_queued_velocity.x, 0, current_queued_velocity.z) * _delta
+	var step_transform_with_clearance: Transform3D = player.global_transform.translated(expected_move_motion + Vector3(0,max_step_up_height,0))
+
+	# Run a body_test_motion slightly above the pos we expect to move to, towards the floor.
+	#  We give some clearance above to ensure there's ample room for the player.
+	#  If it hits a step <= max_step_up_height, we can teleport the player on top of the step
+	#  along with their intended motion forward.
+	var down_check_result = KinematicCollision3D.new()
+	if (!player.test_move(step_transform_with_clearance, Vector3(0,-max_step_up_height *2,0), down_check_result)):
+		return	false# didn't hit anything abort
+
+	if(_draw_step_debug):
+		DebugDraw3D.draw_sphere(down_check_result.get_position(),0.05, Color(0,1,0))
+
+	#check if the found step is too hight	
+	var found_step_height : float = ((step_transform_with_clearance.origin + down_check_result.get_travel()) - player.global_position).y
+	if(found_step_height > max_step_up_height || found_step_height < 0.01):
+		return false
+
+	#did the collision happen low enough to our current ground	
+	if( (down_check_result.get_position() - player.global_position).y > max_step_up_height):
+		return false
+
+	#check if the surface we are went to step on is too step	
+	step_ray_down.global_position = down_check_result.get_position() + Vector3(0,max_step_up_height,0) + (expected_move_motion.normalized() *0.05)
+	if(_draw_step_debug):
+		DebugDraw3D.draw_sphere(step_ray_down.global_position,0.05,Color(1,0,1))
+		DebugDraw3D.draw_line(step_ray_down.global_position , step_ray_down.global_position + (down_check_result.get_normal() * 0.25),Color(1,0,1),0.05)
+
+	step_ray_down.force_raycast_update()
+	if(step_ray_down.is_colliding() and !_is_surface_too_steep(step_ray_down.get_collision_normal())):
+		if(_draw_step_debug):
+			DebugDraw3D.draw_sphere(step_transform_with_clearance.origin + down_check_result.get_travel(),0.05,Color(1,0,0))
+		player.global_position = step_transform_with_clearance.origin + down_check_result.get_travel()
+		player.apply_floor_snap()
+		return true
+	return false
+	
 func _on_movement_input(_event : InputEvent) -> void:
 	if Input.get_vector(MOVE_LEFT, MOVE_RIGHT, MOVE_BACK, MOVE_FORWARD):
 		input_direction = Input.get_vector(MOVE_LEFT, MOVE_RIGHT, MOVE_BACK, MOVE_FORWARD)
@@ -130,6 +179,7 @@ func _handle_player_move(_delta : float, current_active_state : PlayerState):
 	else:
 		current_horizontal_velocity = new_horizontal_target
 	
+	#print("new_horizontal_target " , current_horizontal_velocity)
 	var target_vel : Vector3 = (player.transform.basis * Vector3(current_horizontal_velocity.x, 0, -current_horizontal_velocity.y))
 	_add_velocity(target_vel)
 	
