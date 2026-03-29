ee8730efbfe369ba22685854d1cd1e47b8dc3b3b
VerhelstJoris <megajorver@gmail.com>
2026-03-29 17:56:21 +0100

    further work on path tool, evenly fill out segments along path with posts

--- Scripts/Tools/Terrain/TerrainPathTool.gd
+++ Scripts/Tools/Terrain/TerrainPathTool.gd
@@ -9,12 +9,17 @@ extends Path3D
 
 @export var spacing: float = 0.3
 @export var height_offset : float = 0.0
-@export var enable_random_rotation := true
+
+@export_group("Rotation")
+@export var point_up : bool = false
+@export var enable_random_rotation : bool = true
 
 
 @export_tool_button("Regenerate Path", "Callable") var regenerate_action = _regenerate
 
 var is_updating : bool = false
+var point_alpha_distance_dictionary : Dictionary[int, float]
+
 
 func _regenerate() -> void:
 	if is_updating:
@@ -73,39 +78,35 @@ func spawn_fences() -> void:
 		child.queue_free()
 
 	created_path_objects.set_global_transform(get_global_transform())
-
-
+	
 	if curve.point_count == 0:
 		push_error("Path has no points")
 		return
 		
-	var point_distance_map : Dictionary[int, float] =  _determine_points_distance()		
+	_determine_points_distance()
+	
+	var full_distance : float = curve.get_baked_length()
+	
+	if(point_alpha_distance_dictionary.is_empty() || point_alpha_distance_dictionary.keys().size() != curve.point_count):
+		push_error("Point Distance Map filled in incorrectly!")
+		return
 
-	for current_id in range(curve.point_count):
-		var pos : Vector3= curve.get_point_position(current_id)
-		var next_pos : Vector3 = curve.get_point_position( (current_id +1) % curve.point_count)
-			
-		var direction : Vector3 = (next_pos - pos).normalized()
-		_create_object_at(fence_scene1,  pos, direction)
-
-	# var current_distance : float = 0.0
-	# var current_point_id : int = 0	
-
-	#while current_distance < curve.get_baked_length():
-	#	var pos : Vector3= curve.sample_baked(current_distance)
-	#	var next_pos : Vector3 = curve.sample_baked(current_distance + 0.1)
-	#	
-	#	var terrain_height : float = get_terrain_height(pos)
-	#	if !is_nan(terrain_height) and !is_inf(terrain_height):
-	#		pos.y = terrain_height + height_offset  # Pas de hoogte van het hek aan op de terrain hoogte
-	#	var direction : Vector3 = (next_pos - pos).normalized()
-	#	direction.y = 0
-	#	_create_object_at(fence_scene1,  pos, direction)
-	#	current_distance += spacing
+	for id in range(curve.point_count -1 ):
+		_fill_curve_segment(id, id +1, full_distance, false)
 		
-func _determine_points_distance() -> Dictionary[int,float]:
-	var ret_data : Dictionary[int,float]
-	ret_data[0] = 0.0
+	if(curve.closed):
+		_fill_curve_segment(curve.point_count - 1,0, full_distance, true)
+	else:
+		if(curve.point_count > 1):
+			#create end segment
+			var last_pos : Vector3 = curve.get_point_position(curve.point_count -1)
+			var prev_pos : Vector3 = curve.get_point_position(curve.point_count -2)
+			var direction : Vector3 = (prev_pos - last_pos).normalized()
+			_create_object_at(fence_scene1,  _determine_object_pos_on_terrain(last_pos), direction)
+
+
+func _determine_points_distance() -> void:
+	point_alpha_distance_dictionary[0] = 0.0
 	
 	var current_point_id : int = 1
 	var current_point_to_check : Vector3 = curve.get_point_position(current_point_id)
@@ -117,22 +118,64 @@ func _determine_points_distance() -> Dictionary[int,float]:
 
 	var current_distance : float = 0.0
 	while current_distance < full_length:
-		current_distance += 0.5
+		current_distance += check_interval_distance
 		var sampled_point : Vector3= curve.sample_baked(current_distance)
 		if(sampled_point.distance_squared_to(current_point_to_check) < check_interval_distance_squared):
-			ret_data[current_point_id] = current_distance
+			point_alpha_distance_dictionary[current_point_id] = current_distance
 			print("point ID ", current_point_id, " at distance ", current_distance , " (", current_distance/ full_length," )")
 			if(current_point_id == curve.point_count-1):	#this was the last point
-				return ret_data	
+				return
 
 			current_point_id = current_point_id+1
 			current_point_to_check = curve.get_point_position(current_point_id)
+			
+func _fill_curve_segment(start_id : int, end_id, _full_dist : float, end_segment : bool) -> void:
+	var pos : Vector3= curve.get_point_position(start_id)
+	var end_pos : Vector3 = curve.get_point_position(end_id)
+	
+	var distance_on_curve_between : float = 0 
+	if(end_segment):
+		distance_on_curve_between = _full_dist - point_alpha_distance_dictionary[start_id]
+	else:
+		distance_on_curve_between=  (point_alpha_distance_dictionary[end_id] - point_alpha_distance_dictionary[start_id])	
+	
+	#create object at start
+	var direction : Vector3 = (end_pos - pos).normalized()
+	_create_object_at(fence_scene1,  _determine_object_pos_on_terrain(pos), direction)
+	
+	var amount_needed_raw : float = distance_on_curve_between / spacing	# this will have a decimal point, we need to round this to the closest full integer
+	var amount_needed_rounded : int = roundf(amount_needed_raw)
+	
+	var distance_per_item_avg_needed : float =  distance_on_curve_between / amount_needed_rounded
+	print("filling segment between ID ", start_id , " AND ", end_id, ", dist: ", distance_on_curve_between , " of total ", _full_dist , " with ", amount_needed_rounded , " items, distance per item ", distance_per_item_avg_needed)
+
+	for seg_id in range(1,amount_needed_rounded):
+		var seg_dist_on_curve : float = point_alpha_distance_dictionary[start_id] + (seg_id * distance_per_item_avg_needed)
+		var next_seg_dist_on_curve : float = point_alpha_distance_dictionary[start_id] + ((seg_id +1) * distance_per_item_avg_needed)
+		var seg_pos : Vector3 = curve.sample_baked(seg_dist_on_curve)
+		var next_seg_pos : Vector3 = curve.sample_baked(next_seg_dist_on_curve)
+		
+		var seg_direction : Vector3 = (next_seg_pos - seg_pos).normalized()
+
+		_create_object_at(fence_scene1, _determine_object_pos_on_terrain(seg_pos), seg_direction)
+		
 
 
-	return ret_data
+func _determine_object_pos_on_terrain(world_pos : Vector3) -> Vector3:
+	var new_pos : Vector3 = world_pos
+	
+	var terrain_height : float = get_terrain_height(new_pos)
+	if !is_nan(terrain_height) and !is_inf(terrain_height):
+		new_pos.y = terrain_height + height_offset
+		
+	return new_pos	
+
 
 func _create_object_at(packed_scene: PackedScene, pos: Vector3, _look_at : Vector3) -> void:
 	var new_instance: Node = packed_scene.instantiate()
+
+	if(point_up):
+		_look_at.y = 0
 
 	if _look_at.length() > 0:
 		var new_basis : Basis = Basis.looking_at(_look_at, Vector3.UP)
@@ -146,7 +189,6 @@ func _create_object_at(packed_scene: PackedScene, pos: Vector3, _look_at : Vecto
 
 	if enable_random_rotation:
 		new_instance.rotate_y(randf_range(0.0, 2.0 * PI))
-	
-
+		
 func randf_range(min_val: float, max_val: float) -> float:
 	return min_val + (max_val - min_val) * randf()
