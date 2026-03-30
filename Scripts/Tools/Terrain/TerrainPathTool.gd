bc3419c74e20f9cd41eb32709c941d6eecda91df
VerhelstJoris <megajorver@gmail.com>
2026-03-30 20:12:35 +0100

    first rough version of spawning beams

--- Scripts/Tools/Terrain/TerrainPathTool.gd
+++ Scripts/Tools/Terrain/TerrainPathTool.gd
@@ -2,6 +2,8 @@
 extends Path3D
 
 @export var fence_scene1 : PackedScene
+@export var fence_beam_scene : PackedScene
+@export var default_beam_length : float = 10.0
 
 @export var created_path_objects : Node3D
 @export var terrain: Terrain3D
@@ -26,33 +28,10 @@ func _regenerate() -> void:
 		return
 	is_updating = true
 
-	adjust_curve_with_terrain_data()
 	spawn_fences()
 
 	is_updating = false
-
-func adjust_curve_with_terrain_data() -> void:
-	if terrain == null:
-		push_error("No Valid Terrain3D")
-		return
-
-	if curve.point_count == 0:
-		push_error("Path has no points")
-		return
-
-	print("Path3D has", curve.point_count, "points, start adjustment")
-
-	for i in range(curve.point_count):
-		var point: Vector3 = curve.get_point_position(i)
-		
-		var terrain_height : float = get_terrain_height(point)
-#
-		if !is_nan(terrain_height) and !is_inf(terrain_height):
-			var new_point = Vector3(point.x, terrain_height + height_offset, point.z)
-			curve.set_point_position(i, new_point)
-		else:
-			push_error("NO valid height for" + str(point) + ", fallback to previous")
-
+	
 func get_terrain_height(point_pos: Vector3) -> float:
 	if terrain == null or terrain.data == null:
 		push_error("no valid Terrain3D or Terrain3D Data")
@@ -78,6 +57,8 @@ func spawn_fences() -> void:
 		child.queue_free()
 
 	created_path_objects.set_global_transform(get_global_transform())
+	var target_pos : Vector3 = get_global_position()
+	created_path_objects.set_global_position( Vector3(target_pos.x, 0, target_pos.z))
 	
 	if curve.point_count == 0:
 		push_error("Path has no points")
@@ -103,8 +84,7 @@ func spawn_fences() -> void:
 			var prev_pos : Vector3 = curve.get_point_position(curve.point_count -2)
 			var direction : Vector3 = (prev_pos - last_pos).normalized()
 			_create_object_at(fence_scene1,  _determine_object_pos_on_terrain(last_pos), direction)
-
-
+			
 func _determine_points_distance() -> void:
 	point_alpha_distance_dictionary[0] = 0.0
 	
@@ -143,23 +123,46 @@ func _fill_curve_segment(start_id : int, end_id, _full_dist : float, end_segment
 	var direction : Vector3 = (end_pos - pos).normalized()
 	_create_object_at(fence_scene1,  _determine_object_pos_on_terrain(pos), direction)
 	
-	var amount_needed_raw : float = distance_on_curve_between / spacing	# this will have a decimal point, we need to round this to the closest full integer
-	var amount_needed_rounded : int = roundf(amount_needed_raw)
+	var amount_needed_rounded : int = round(distance_on_curve_between / spacing)
 	
 	var distance_per_item_avg_needed : float =  distance_on_curve_between / amount_needed_rounded
+	var beam_scale_mult : float = distance_per_item_avg_needed / default_beam_length
+
 	print("filling segment between ID ", start_id , " AND ", end_id, ", dist: ", distance_on_curve_between , " of total ", _full_dist , " with ", amount_needed_rounded , " items, distance per item ", distance_per_item_avg_needed)
+
+	var prev_seg_pos : Vector3 = pos
 
 	for seg_id in range(1,amount_needed_rounded):
 		var seg_dist_on_curve : float = point_alpha_distance_dictionary[start_id] + (seg_id * distance_per_item_avg_needed)
 		var next_seg_dist_on_curve : float = point_alpha_distance_dictionary[start_id] + ((seg_id +1) * distance_per_item_avg_needed)
 		var seg_pos : Vector3 = curve.sample_baked(seg_dist_on_curve)
+
+		# make this post face what would be the next one
 		var next_seg_pos : Vector3 = curve.sample_baked(next_seg_dist_on_curve)
-		
-		var seg_direction : Vector3 = (next_seg_pos - seg_pos).normalized()
+		var new_seg_direction : Vector3 = (next_seg_pos - seg_pos).normalized()
 
-		_create_object_at(fence_scene1, _determine_object_pos_on_terrain(seg_pos), seg_direction)
+		var new_segment_pos : Vector3 = _determine_object_pos_on_terrain(seg_pos)
 		
+		#create a post at the new spot
+		_create_object_at(fence_scene1,new_segment_pos, new_seg_direction)
+		
+		if(fence_beam_scene):
+			#create a beam in between the previous and this post
+			var beam : Node3D = _create_object_at(fence_beam_scene,(new_segment_pos + prev_seg_pos) /2, new_seg_direction)
+			
+			if(beam):
+				beam.set_scale(Vector3(1, 1,beam_scale_mult))
+		
+		prev_seg_pos = new_segment_pos
+		
+	#create a beam at the very final segment	
+	if(fence_beam_scene):
+		#create a beam in between the previous and this post
+		var beam : Node3D = _create_object_at(fence_beam_scene,(prev_seg_pos + end_pos) /2, (prev_seg_pos - end_pos).normalized())
 
+		if(beam):
+			beam.set_scale(Vector3(1, 1,beam_scale_mult))
+		
 
 func _determine_object_pos_on_terrain(world_pos : Vector3) -> Vector3:
 	var new_pos : Vector3 = world_pos
@@ -169,9 +172,8 @@ func _determine_object_pos_on_terrain(world_pos : Vector3) -> Vector3:
 		new_pos.y = terrain_height + height_offset
 		
 	return new_pos	
-
-
-func _create_object_at(packed_scene: PackedScene, pos: Vector3, _look_at : Vector3) -> void:
+	
+func _create_object_at(packed_scene: PackedScene, pos: Vector3, _look_at : Vector3) -> Node3D:
 	var new_instance: Node = packed_scene.instantiate()
 
 	if(point_up):
@@ -190,5 +192,7 @@ func _create_object_at(packed_scene: PackedScene, pos: Vector3, _look_at : Vecto
 	if enable_random_rotation:
 		new_instance.rotate_y(randf_range(0.0, 2.0 * PI))
 		
+	return new_instance
+	
 func randf_range(min_val: float, max_val: float) -> float:
 	return min_val + (max_val - min_val) * randf()
